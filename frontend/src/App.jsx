5f290e0a78fcaa76328fc4665b0d7e88c3c5e3ef
kone-net <kone_net@163.com>
2025-09-27 00:23:59 +0800

    feat: change base url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
 import { useAccount, useBalance, useReadContract, useWriteContract, useWaitForTransactionReceipt } from 'wagmi'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import './App.css'
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web3-game/',
+  base: '/web3-game/', // 与package.json中的homepage路径匹配
   build: {
     outDir: '../docs', // 默认是 dist，可修改为自定义路径（如 'build'、'public' 等）
+  },
+  resolve: {
+    alias: {
+      '/@': '/src'
+    }
   }
 })
