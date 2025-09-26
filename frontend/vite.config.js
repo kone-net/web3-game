3fb3d63acc10c73825c66805466430779ec35839
kone-net <kone_net@163.com>
2025-09-26 23:35:48 +0800

    feat: change vite base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: 'https://kone-net.github.io/web3-game/',
   build: {
     outDir: '../docs', // 默认是 dist，可修改为自定义路径（如 'build'、'public' 等）
   }
